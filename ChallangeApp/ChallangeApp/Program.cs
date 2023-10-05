9924c72c2681719165c8f7f92d5664ab1a7e0011
Jakub Rychlicki <jakubrychlicki@hotmail.com>
2023-10-05 14:18:37 +0200

    Zadanie dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "K";
+var age = 33;
+
+if (sex == "K" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (sex != "K" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Męzczyzna");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
